6284c772b223339c5551d1ef17b1aa72b8779301
johntr <jtredlich@gmail.com>
2015-03-03 23:09:18 -0500

    added formatted table and working on calculations

--- retirement.pl
+++ retirement.pl
@@ -1,4 +1,5 @@
 #!/usr/local/bin/perl
+use Time::Piece;
 print "Welcome to your friendly retirement calculator!\n";
 print "Please enter your current retirement balance. \$";
 chomp($starting = <>);
@@ -12,3 +13,15 @@ print "Please enter the percentage return you expect: ";
 chomp($returnrate = <>);
 print "Enter amount you wish to reitre with. \$";
 chomp ($retirethresh = <>);
+$years = $retireage - $age;
+$t = Time::Piece->new();
+$curryear = $t->year;
+printf("%5s%10s%10s%15s\n", "Year", "Balance", "Interest", "New Balance");
+$i = 0;
+while ($age <= $retireage) {
+  $interest = $starting*$returnrate;
+  $yearend = $starting+$interest;
+  printf("%5s%10s%10s%15s\n", $curryear+$i, $starting, $interest,$yearend );
+  $i++;
+  $age++;
+}
