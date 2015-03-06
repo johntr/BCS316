6227f7589f26ab64648b191b131b955900bb3f91
johntr <jtredlich@gmail.com>
2015-03-05 20:59:06 -0500

    Fixed table data and added check if you passed retirement thresh.

--- retirement.pl
+++ retirement.pl
@@ -11,17 +11,36 @@ print "Please enter dollar amount you will deposit each year. \$";
 chomp($yearlycontrib = <>);
 print "Please enter the percentage return you expect: ";
 chomp($returnrate = <>);
+$returnrate = $returnrate/100;
 print "Enter amount you wish to reitre with. \$";
 chomp ($retirethresh = <>);
 $years = $retireage - $age;
 $t = Time::Piece->new();
 $curryear = $t->year;
-printf("%5s%10s%10s%15s\n", "Year", "Balance", "Interest", "New Balance");
+printf("%5s%13s%13s%17s\n", "Year", "Balance(\$)", "Interest(\$)", "New Balance(\$)");
 $i = 0;
 while ($age <= $retireage) {
-  $interest = $starting*$returnrate;
-  $yearend = $starting+$interest;
-  printf("%5s%10s%10s%15s\n", $curryear+$i, $starting, $interest,$yearend );
+  if($age == $retireage) {
+    printf("%5s   %.2f\n", $curryear+$i, $starting);
+    last;
+  }
+  $interest = $starting * $returnrate;
+  $yearend = $starting + $interest;
+  printf("%5s   %.2f     %.2f       %.2f\n", $curryear+$i, $starting, $interest,$yearend );
+  $starting = $yearend + $yearlycontrib;
   $i++;
   $age++;
 }
+
+if($retirethresh < $starting){
+  print "Congradulations you can retire!!!!\n"
+}
+elsif($retirethresh > $starting){
+  $num = 10;
+  while($num--){
+    sleep(1);
+  }
+  print "Boom!!!!..... YOU WILL BE A POOR OLD PERSON!!!!\n";
+
+}
+
